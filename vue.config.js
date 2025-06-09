a6c33954a51c764712e7a5c6d0764ae3090df7bf
lwstkhyl <1271943237@qq.com>
2025-06-09 13:37:25 +0800

    打包优化

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,50 @@
 const { defineConfig } = require('@vue/cli-service')
+const TerserPlugin = require('terser-webpack-plugin')
+let productionGzipExtensions = /\.(js|css|json|txt|html|ico|svg)(\?.*)?$/i;
 module.exports = defineConfig({
   transpileDependencies: true,
   // runtimeCompiler: true,
+  chainWebpack: config => {
+    config.plugin("CompressionPlugin").use('compression-webpack-plugin', [{
+      filename: '[path][base].gz',
+      algorithm: 'gzip',
+      // 要压缩的文件（正则）
+      test: productionGzipExtensions,
+      // 最小文件开启压缩
+      threshold: 10240,
+      minRatio: 0.8
+    }])
+  },
+  configureWebpack: (config) => {
+    let optimization = {
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            ecma: undefined,
+            warnings: false,
+            parse: {},
+            compress: {
+              drop_console: true,
+              drop_debugger: true,
+              pure_funcs: ['console.log'] // 移除console
+            }
+          }
+        })
+      ],
+      runtimeChunk: 'single',
+      splitChunks: {
+        chunks: 'all',
+        maxInitialRequests: Infinity,
+        minSize: 20000,
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/
+          }
+        }
+      }
+    }
+    Object.assign(config, {
+      optimization
+    })
+  },
 })
